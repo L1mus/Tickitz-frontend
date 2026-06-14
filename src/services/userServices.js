484611ad32cd241166e20f24af06a7307131a015
aqilknz <khairunnz123@gmail.com>
2026-06-14 13:01:58 +0700

    chore: remove interceptor in userService

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,65 +1,17 @@
-import axios from "axios";
-
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api";
-
-
-const userApi = axios.create({
-    baseURL: BASE_URL,
-    // headers: { "Content-Type": "application/json" },
-});
-
-let store;
-export const injectStore = (_store) => {
-    store = _store
-}
-
-const getCleanToken = () => {
-    try {
-        // const persistData = localStorage.getItem("persist:auth");
-        // if (persistData) {
-        //     const authState = JSON.parse(persistData || "{}");
-        //     let token = authState?.token;
-
-        //     if (token) {
-            
-        //         return token.replace(/^"|"$/g, '');
-        //     }
-        // }
-
-        if (!store) return null
-        const state = store.getState();
-
-        let token = state.auth?.token
-        if (token) {
-            return token.replace(/^"|"$/g, '')
-        }
-    } catch (error) {
-        console.error("Failed to parse local token", error);
-    }
-    return null;
-};
-
-userApi.interceptors.request.use(
-    (config) => {
-        const token = getCleanToken();
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
-);
-
+import userApi from "./api.js"
 export const getProfileAPI = async () => {
     const response = await userApi.get("/users/profile");
     return response.data;
 };
 
 export const updateProfileAPI = async (payload) => {
-    const response = await userApi.patch("/users/profile", payload);
+    const response = await userApi.patch("/users/profile", payload, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    });
     return response.data;
 };
-
 
 export const getOrderHistoryAPI = async () => {
     const response = await userApi.get("/users/history");
